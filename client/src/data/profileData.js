a8ad8fe99e671c2d977dbfe1a89504f6780cb3b7
Amardeep Dwivedi <amardeepdwivedi1494@gmail.com>
2025-11-05 23:26:41 +0530

    fix: update resume

--- client/src/data/profileData.js
+++ client/src/data/profileData.js
@@ -1,7 +1,7 @@
 import profileImage from "../assets/profile_image.jpg";
 const profileData = {
   image: profileImage,
-  resume: "/amardeep_dev_2YOE.pdf",
+  resume: "/amardeep_dev_2+YOE.pdf",
   projects: [
     {
       id: 1,
