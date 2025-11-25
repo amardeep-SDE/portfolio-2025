856b351261cb2c60016bf1fe455f1d23ec59a1f8
Amardeep Dwivedi <amardeepdwivedi1494@gmail.com>
2025-11-25 23:45:07 +0530

    fix: layout issue

--- client/src/Layout/Layout.jsx
+++ client/src/Layout/Layout.jsx
@@ -4,13 +4,16 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <>
-      <Navbar />
-      <main className="min-h-[80vh]">
-        <Outlet />
-      </main>
-      <Footer />
-    </>
+   <>
+  <div className="overflow-x-hidden">
+    <Navbar />
+    <main className="min-h-[80vh]">
+      <Outlet />
+    </main>
+    <Footer />
+  </div>
+</>
+
   );
 };
 
